4d48ebc536e3c358dab9588544cd9d9892d7e14b
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-02 14:14:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -104,17 +104,7 @@
                     </button>
                     <a class="navbar-brand" href="#">Home</a>
                 </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-dashboard"></i>
-								<p class="hidden-lg hidden-md">Dashboard</p>
-                            </a>
-                        </li>
-                    </ul>
-                    
-                </div>
+               
             </div>
         </nav>
 
