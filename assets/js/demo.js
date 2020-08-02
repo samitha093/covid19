37e2a5df76ab5fdf4116b5501dc58a55d5978793
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-02 15:26:00 +0530

    Update demo.js

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -247,7 +247,7 @@ demo = {
 
     	$.notify({
         	icon: "pe-7s-gift",
-        	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+        	message: "Welcome to <b>Covid 19 Public Distribution System</b> - From team x."
 
         },{
             type: type[color],
