7fa65473fb1d655ded39610d42d2dd0a0f18cfb6
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-02 14:37:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Home</a>
+                    <a class="navbar-brand" href="#">Covid 19 Public Distribution System</a>
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-left">
