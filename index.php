ebcc8d6c957278deadc06eed26693bbd01c551d3
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-08 07:01:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,20 +20,16 @@ require_once("includes/sidebar.php");
                                 <h4 class="title">Users Behaviors</h4>
                             </div>
 
-                            <div class="content">
-                                <div id="chartHours" class="ct-chart"></div>
-                                <div class="footer">
-                                    <div class="legend">
-                                        <i class="fa fa-circle text-info"></i> Open
-                                        <i class="fa fa-circle text-danger"></i> Click
-                                        <i class="fa fa-circle text-warning"></i> Click Second Time
-                                    </div>
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-history"></i> Updated 3 minutes ago
-                                    </div>
-                                </div>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Users Behaviors</h4>
                             </div>
+
                             
                         </div>
                     </div>
