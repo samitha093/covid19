6a0d9a021ea9e7b233bfb798bdff667cebe6021a
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-08 06:58:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,25 +10,7 @@ require_once("includes/sidebar.php");
                         <div class="card">
                          
                             <Img src="unnamed.png">
-                            <div class="header">
-                                <h4 class="title">Email Statistics</h4>
-                                <p class="category">Last Campaign Performance</p>
-                            </div>
-                            <div class="content">
-                                <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
-
-                                <div class="footer">
-                                    <div class="legend">
-                                        <i class="fa fa-circle text-info"></i> Open
-                                        <i class="fa fa-circle text-danger"></i> Bounce
-                                        <i class="fa fa-circle text-warning"></i> Unsubscribe
-                                    </div>
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
 
@@ -36,8 +18,8 @@ require_once("includes/sidebar.php");
                         <div class="card">
                             <div class="header">
                                 <h4 class="title">Users Behavior</h4>
-                                <p class="category">24 Hours performance</p>
                             </div>
+
                             <div class="content">
                                 <div id="chartHours" class="ct-chart"></div>
                                 <div class="footer">
@@ -52,6 +34,7 @@ require_once("includes/sidebar.php");
                                     </div>
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
 
